df2e105b990f4eaa92953af36b5ef91b4e56e500
Aaron Cox <aaron@greymass.com>
2022-12-31 02:08:12 -0500

    allow console.log in example

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {AbstractTransactPlugin, TransactContext, TransactHookTypes} from '@wharf
 export class TransactPluginTemplate extends AbstractTransactPlugin {
     register(context: TransactContext): void {
         context.addHook(TransactHookTypes.beforeSign, async (request, context) => {
+            // eslint-disable-next-line no-console
             console.log('beforeSign hook called with', request, context)
             return {
                 request,
